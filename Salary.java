b597ef653b79e5cfdbb45e314622101d4b7c132a
Henri Lam <lamhenri0@gmail.com>
2025-02-28 14:56:10 -0500

    Problem 14 is fixed

--- Salary.java
+++ Salary.java
@@ -7,15 +7,16 @@
  */
 public class Salary
 {
-    public void salary(int workedHours, int hourlyWage)
+    private double totalsalary;
+    private double netpay;
+    
+    public void salary(int workedHours, double hourlyWage)
     {
-        /*
         
-         totalsalary = workedHours*hourlyWage;
-         System.out.println("Your total salary is: " + totalsalary)
-         netpay = totalsalary - (totalsalary*0.3);
-         System.out.println("Your netpay is: " + netpay)
+        totalsalary = workedHours*hourlyWage;
+        System.out.println("Your total salary is: " + totalsalary);
+        netpay = totalsalary - (totalsalary*0.3);
+        System.out.println("Your netpay is: " + netpay);
          
-         */
     }
 }
