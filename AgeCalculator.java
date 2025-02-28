d8601fcd93e08302389409916728d079ba210fca
Henri Lam <lamhenri0@gmail.com>
2025-02-28 14:57:50 -0500

    Problem 15 is fixed.

--- AgeCalculator.java
+++ AgeCalculator.java
@@ -8,25 +8,22 @@
 public class AgeCalculator
 {
     private int currentYear = 2025;
+    private int age;
     
-    /*
-
     public int age(int yearOfBirth)
     {
           age = currentYear - yearOfBirth;
         
           if(age >= 16)
           {
-              System.out.println("You can drive.")
+              System.out.println("You can drive.");
           }
           
           else
           {
-              System.out.println("You can drive in " + (16-age) + " years.")
+              System.out.println("You can drive in " + (16-age) + " years.");
           }
           
           return age;
     }
-    
-    */
 }
